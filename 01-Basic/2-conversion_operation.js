bddb640a84017b17e9f5a4ee3c29cce4a968fbc7
Simran <129810121+simR122@users.noreply.github.com>
2024-08-30 11:17:44 +0000

    basic 2

---
+++ 01-Basic/2-conversion_operation.js
@@ -0,0 +1,41 @@
+let score = true 
+// console.log(typeof score)
+
+let scoreInNumber = Number(score)
+
+// console.log(scoreInNumber)
+// For what value of score it will give-
+
+// "" => 0
+// "20asd"=> NaN
+// "bjf" => NaN
+// true => 1
+
+// CHECK FOR BOOLEAN 
+let IsLoggedIn = ""
+
+let BooleanIsLoggedIn = Boolean(IsLoggedIn)
+
+// console.log(BooleanIsLoggedIn) 
+// 0 => false 
+// 34 => true 
+// "str" => true
+// "" => false
+
+
+
+// CHECK FOR STRING
+let variable = ""
+let stringVariable = String(variable)
+
+console.log(stringVariable)
+console.log(typeof(stringVariable))
+
+// "23" => String 23
+//  23 => string 23
+
+// "" => string blank**
+
+
+
+
