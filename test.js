7dd38b849f728cd695712353976048554dfca0a1
Simran <129810121+simR122@users.noreply.github.com>
2024-08-30 10:28:08 +0000

    Datatypes

--- test.js
+++ test.js
@@ -1,1 +1,41 @@
 console.log("SIMRAN")
+
+// # variables
+
+// const, var, let
+const rollNo = 12
+var studentName = "SIMRAN" //Problem is "Scope not defined"
+let name = "SIMR"
+
+// Also
+classTeacher = "MRS. SUNITA" //possible
+
+let position; // BY default == UNdefined
+
+console.table([rollNo, studentName, classTeacher, position])
+
+// #########################################
+
+
+// DATA TYPES
+
+"use script" //treat all JS code as new version 
+
+//alert(3+3) //we are using node Not Browser
+
+//number
+//bigint
+//string
+// boolean
+// null ->Stand alone value
+// undefined
+// symbol ->for uniqueness in react
+
+
+// ## Object
+
+console.log(typeof rollNo)
+
+//*** IMP
+console.log(typeof null) //ouput is object
+console.log(typeof undefined)//output is undefined
